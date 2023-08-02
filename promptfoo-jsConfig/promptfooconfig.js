1490608d4426c8b664948f4cb8c7cf2bb0932300
Abdullah Raghib <abdullah.raghib@vidyamantra.com>
2023-08-02 13:47:47 +0530

    3 questions prototype

--- promptfoo-jsConfig/promptfooconfig.js
+++ promptfoo-jsConfig/promptfooconfig.js
@@ -20,6 +20,33 @@ module.exports = {
       },
     ]
     },
+    {
+      vars: {
+        question: `The soft silvery metalic element which ionizes easily when heated or exposed to light and is present in atomic clocks is :
+
+        (1) Cerium (2) Cesium
+        (3) Calcium (4) Califonrium`
+      },
+      assert: [
+        {
+        type: "similar",
+        value: `{"subjectTags": [{"Level 1": "Chemistry", "Level 2": "In-Organic Chemistry", "Level 3": "Elements"}], "Skills": ["Recognizing	 Knowledge of specific details and elements"]}`,
+        threshold: 0.8
+      },
+    ]
+    },
+    {
+      vars: {
+        question: `In a certain code language, 'mee muk pic' is 'roses are yellow', 'nil dic' is 'white flowers' and 'pic muk dic' is 'flowers are fruits'. What is the code for 'white' in that code language?`
+      },
+      assert: [
+        {
+        type: "similar",
+        value: `{"subjectTags": [{"Level 1": "Reasoning", "Level 2": "Verbal Reasoning", "Level 3": "Coding and Decoding"}], "Skills": ["Interpreting"]}`,
+        threshold: 0.8
+      },
+    ]
+    },
     
   ],
   outputPath: "output.json"
